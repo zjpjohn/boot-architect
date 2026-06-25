8a8fb4669f8e8a1474e04fa295eb0acc4e08d4ae
zjp123 <zjprevenge@126.com>
2026-06-25 23:00:33 +0800

    refactor(architect-spring): Executor 发现机制从 @ExecPoint 注解改为 @IndexSubclasses 自动索引
    - 删除 ExecPoint 注解，Executor 接口加 @IndexSubclasses 实现自动子类发现
    - ExecutorFactory 使用 ClassIndex.getSubclasses() 替代 getAnnotated()
    - web-example 恢复 ExecutorController/Service，EnumExecutorFactory → ExecutorFactory

--- architect-spring/src/main/java/com/cloud/arch/executor/Executor.java
+++ architect-spring/src/main/java/com/cloud/arch/executor/Executor.java
@@ -1,5 +1,8 @@
 package com.cloud.arch.executor;
 
+import org.atteo.classindex.IndexSubclasses;
+
+@IndexSubclasses
 public interface Executor<K extends Comparable<K>> {
 
     /**
--- architect-xmodules/web-example/src/main/java/com/boot/architect/domain/ability/GenderExecutor.java
+++ architect-xmodules/web-example/src/main/java/com/boot/architect/domain/ability/GenderExecutor.java
@@ -2,9 +2,7 @@ package com.boot.architect.domain.ability;
 
 import com.boot.architect.infrast.persist.enums.Gender;
 import com.cloud.arch.executor.Executor;
-import com.cloud.arch.executor.ExecPoint;
 
-@ExecPoint
 public interface GenderExecutor extends Executor<Gender> {
 
     void execute();
